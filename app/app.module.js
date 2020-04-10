173de5dcf1eed2a865bf9f54e646c1c68df303ec
Nelson Trujillo <nelsontrujillog@gmail.com>
2020-04-10 17:26:26 -0500

    Components

--- app/app.module.js
+++ app/app.module.js
@@ -1,21 +1,3 @@
 
 // Define the `phonecatApp` module
 var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListController', Controller)
-
-// Define the `PhoneListController` controller on the `phonecatApp` module
-function Controller($scope) {
-    $scope.phones = [
-        {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
-        }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
-        }
-    ];
-}
