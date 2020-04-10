18fa16c0f3246a28d69c63b609abef7acb1b53e6
Nelson Trujillo <nelsontrujillog@gmail.com>
2020-04-10 18:08:52 -0500

    Two-way Data Binding

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,7 +1,7 @@
 angular
     .module('phoneList')
     .component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
-        templateUrl:'phone-list/phone-list.template.html',
+        templateUrl: 'phone-list/phone-list.template.html',
         controller: PhoneListController
     });
 
@@ -9,13 +9,18 @@ function PhoneListController() {
     this.phones = [
         {
             name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.'
+            snippet: 'Fast just got faster with Nexus S.',
+            age: 1
         }, {
             name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.'
+            snippet: 'The Next, Next Generation tablet.',
+            age: 2
         }, {
             name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.'
+            snippet: 'The Next, Next Generation tablet.',
+            age: 3
         }
     ];
+
+    this.orderProp = 'age';
 }
