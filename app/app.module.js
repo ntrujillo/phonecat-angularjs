58273b77319da8a7737534a0fe55bdaa8018177e
Nelson Trujillo <nelsontrujillog@gmail.com>
2020-04-10 19:24:02 -0500

    Custom Filters

--- app/app.module.js
+++ app/app.module.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Define the `phonecatApp` module
-angular.module('phonecatApp', ['ngRoute', 'phoneDetail', 'phoneList']);
+angular.module('phonecatApp', ['ngRoute', 'core', 'phoneDetail', 'phoneList']);
