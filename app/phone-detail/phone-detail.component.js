ce71d324ca958edf8e4f0220d775ffe2891bc9b3
Nelson Trujillo <nelsontrujillog@gmail.com>
2020-04-10 19:28:11 -0500

    Event Handlers

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -11,7 +11,12 @@ angular.
 function PhoneDetailController($http, $routeParams) {
   var self = this;
 
+  self.setImage = function setImage(imageUrl) {
+    self.mainImageUrl = imageUrl;
+  };
+
   $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
     self.phone = response.data;
+    self.setImage(self.phone.images[0]);
   });
 }
